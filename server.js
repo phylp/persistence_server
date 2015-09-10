b276e5d1576aac94952c84f2f24e604a12bd9e31
phylp <philson3@gmail.com>
2015-09-09 22:50:41 -0700

    fixed tabs

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var fs = require('fs');
 
 var counter = 0;
 var server = http.createServer(function(req, res){
-	if(req.url === '/upload' && req.method === 'POST'){
-		counter += 1;
-		req.on('data', function(data){
-			var postData = JSON.parse(data);
-			fs.writeFile(__dirname + '/json_logs/' + counter + '.json', data);
-			res.end();
-		});
-	}
+  if(req.url === '/upload' && req.method === 'POST'){
+    counter += 1;
+    req.on('data', function(data){
+      var postData = JSON.parse(data);
+      fs.writeFile(__dirname + '/json_logs/' + counter + '.json', data);
+      res.end();
+    });
+  }
 
-	if(req.method === 'GET'){
-		res.writeHead(200, {'Content-Type':'text/plain'});
-		res.write('you made a get request');
-		res.end();
-	}
+  if(req.method === 'GET'){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write('you made a get request');
+    res.end();
+  }
 });
 server.listen(3002);
