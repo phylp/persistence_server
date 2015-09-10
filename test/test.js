c571ef7de8ae321155303858d5578d06692280a8
phylp <philson3@gmail.com>
2015-09-09 22:49:18 -0700

    fixed tabs

--- test/test.js
+++ test/test.js
@@ -7,34 +7,34 @@ chai.use(chaiHttp);
 require('../server.js');
 
 describe('server', function(done){
-	var response;
-	var error;
-	before(function(done){
-		chai.request('localhost:3002')
-		.get('/')
-		.end(function(err, res){
-			error = err;
-			response = res;
-			done();
-		});
-	});
-	it('should tell me the method of the request', function(){
-		expect(response.text).to.eql('you made a get request');
-	});
+  var response;
+  var error;
+  before(function(done){
+    chai.request('localhost:3002')
+    .get('/')
+    .end(function(err, res){
+      error = err;
+      response = res;
+      done();
+    });
+  });
+  it('should tell me the method of the request', function(){
+    expect(response.text).to.eql('you made a get request');
+  });
 });
 
 describe('server-post', function(done){
-	var oldDir = fs.readdirSync(__dirname + '/../json_logs').length;	
-	before(function(done){
-		chai.request('localhost:3002')
-		.post('/upload')
-		.send('{"name":"charles"}')
-		.end(function(err, res){
-		done();
-		});
-	});
-	it('should create a new file', function(){
-		var newDir = fs.readdirSync(__dirname + '/../json_logs').length;
-		expect(oldDir).to.not.eql(newDir);
-	});	
+  var oldDir = fs.readdirSync(__dirname + '/../json_logs').length;  
+  before(function(done){
+    chai.request('localhost:3002')
+    .post('/upload')
+    .send('{"name":"charles"}')
+    .end(function(err, res){
+    done();
+    });
+  });
+  it('should create a new file', function(){
+    var newDir = fs.readdirSync(__dirname + '/../json_logs').length;
+    expect(oldDir).to.not.eql(newDir);
+  }); 
 });
